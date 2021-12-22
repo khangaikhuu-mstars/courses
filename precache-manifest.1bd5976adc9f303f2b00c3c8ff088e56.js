1693675117dfb6257e71446b42583ebedff60219
khangaikhuu uvgunkhuu <khangaikhuu@mstars.mn>
2021-12-22 11:43:57 +0800

    Updates

---
+++ precache-manifest.1bd5976adc9f303f2b00c3c8ff088e56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a231a1758a9287eda698",
+    "url": "/courses/static/css/main.9af57a9a.chunk.css"
+  },
+  {
+    "revision": "a231a1758a9287eda698",
+    "url": "/courses/static/js/main.a231a175.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/courses/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "b0914f7499e540a17bff",
+    "url": "/courses/static/js/runtime~main.b0914f74.js"
+  },
+  {
+    "revision": "ad24a9562f58a4e6d86405174f16a514",
+    "url": "/courses/index.html"
+  }
+];
